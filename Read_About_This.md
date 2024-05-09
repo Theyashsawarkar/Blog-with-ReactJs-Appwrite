f5102483395292a734c60e930c951cd5eb2ac7c3
Yash Sawarkar <yashsawarkar01@gmail.com>
2024-05-09 20:32:30 +0530

    added .env file and conf file for easier import of variables

---
+++ Read_About_This.md
@@ -0,0 +1,10 @@
+
+## tools 
+  - tinyMce
+  - html-react-parser
+  - react hook form
+  - appwrite
+
+## How To access the env file and it's variables in create react app setup and vite 
+  - the set up is different for each way 
+
---
+++ src/Conf/Conf.js
@@ -0,0 +1,13 @@
+
+// this way we prevent the case where there is some problem loading the enviroment variable and it makes code easy to write and beutifull to look at 
+// this practice helps alot in typescript too 
+
+const Conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default Conf ;
