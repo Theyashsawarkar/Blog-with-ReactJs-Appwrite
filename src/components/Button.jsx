2232d0bc81083a669bb4ca5bd2f4a2b21c8ef362
Yash Sawarkar <yashsawarkar01@gmail.com>
2024-05-20 15:47:15 +0530

    made button reusable

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,20 @@
-import React from "react";
-
+/* eslint-disable react/prop-types */
 export default function Button({
-  children,
-  type = "button",
-  bgColor = "bg-blue-600",
-  textColor = "text-white",
+  name,
+  clickHandler,
   className = "",
   ...props
 }) {
   return (
-    <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
-      {children}
+    <button
+      onClick={(e) => {
+        e.preventDefault();
+        clickHandler();
+      }}
+      className={className}
+      {...props}
+    >
+      {name}
     </button>
   );
 }
