e317ca8bb2c30d4ef7cdbbe8035539863ee71163
Yash Sawarkar <yashsawarkar01@gmail.com>
2024-05-20 21:01:34 +0530

    ui updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
+import logo from "../assets/logo.png";
+import ListPosts from "../components/ListPosts";
+import Options from "../components/Options";
+
 function Home() {
   return (
-    <div className="w-full min-h-[50vh] flex items-center">
-      <h1 className="text-center text-[5rem] w-full font-bold">HOME PAGE </h1>
+    <div className="w-full min-h-[50vh] flex flex-col items-center">
+      <img src={logo} alt="logo" width={500} className=" mt-[-5rem] " />
+      <span className="text-white text-3xl mt-[-5rem]">
+        Roadmap to DSA Mastery
+      </span>
+      <h1 className="text-center text-white text-[4rem] mt-1 mb-10 font-serif w-full font-bold">
+        {"It's"} <span className="text-[#f97316]">Simpler </span> than You Think
+      </h1>
+      <Options />
+      <ListPosts />
     </div>
   );
 }
