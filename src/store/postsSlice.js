68467457daa9eb447ae1edae1b5eee525c9093ba
Yash Sawarkar <yashsawarkar01@gmail.com>
2024-05-21 15:47:47 +0530

    some slice headache

--- src/store/postsSlice.js
+++ src/store/postsSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  posts: [],
+  allPosts: [],
   activePosts: [],
+  myPosts: [],
 };
 
 const postsSlice = createSlice({
@@ -10,16 +11,37 @@ const postsSlice = createSlice({
   initialState,
   reducers: {
     addPosts: (state, action) => {
-      state.posts = action.payload;
+      // Ensure payload is an array
+      if (!Array.isArray(action.payload.posts)) {
+        console.error("addPosts: Payload must be an array of posts");
+        return; // Or throw an error if preferred
+      }
+      state.allPosts = action.payload.posts;
     },
     addActivePosts: (state, action) => {
       const topic = action.payload;
-      //NOTE: this needs post to have topic attribute in appwrite
-      state.activePosts = state.posts.filter((post) => post.$topic === topic);
+      console.log("action", action.payload);
+      console.log("state", state.activePosts);
+      if (Array.isArray(state.allPosts)) {
+        state.activePosts = state.allPosts.filter(
+          (post) => post.topic === topic
+        );
+      } else {
+        state.activePosts = [];
+      }
+    },
+    addMyPosts: (state, action) => {
+      if (Array.isArray(state.allPosts)) {
+        state.myPosts = state.allPosts.filter(
+          (post) => post.userId === action.payload
+        );
+      } else {
+        state.myPosts = [];
+      }
     },
   },
 });
 
-export const { addActivePosts, addPosts } = postsSlice.actions;
+export const { addActivePosts, addPosts, addMyPosts } = postsSlice.actions;
 
 export default postsSlice.reducer;
