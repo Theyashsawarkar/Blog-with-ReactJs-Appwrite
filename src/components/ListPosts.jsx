1608334cbc3388036e79d356cd502b0e0618b864
Yash Sawarkar <yashsawarkar01@gmail.com>
2024-05-22 10:12:02 +0530

    width reduced to 90%

--- src/components/ListPosts.jsx
+++ src/components/ListPosts.jsx
@@ -6,12 +6,12 @@ function ListPosts() {
   console.log("active Posts ::", activePosts);
 
   return (
-    <div className="flex justify-center flex-wrap w-full mt-10">
+    <div className="flex justify-center flex-wrap w-[90%] mt-10">
       {!activePosts ? (
         <h1>No posts yet</h1>
       ) : (
         activePosts.map((post) => (
-          <div key={post.$id} className="p-2 w-1/5">
+          <div key={post.$id} className="p-2 w-1/5 ">
             <PostCard {...post} />
           </div>
         ))
