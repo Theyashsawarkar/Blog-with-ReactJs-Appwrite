bf8c805eeebf3fcbb06dd763b9e45289579ad81f
Yash Sawarkar <yashsawarkar01@gmail.com>
2024-05-22 10:17:41 +0530

    handle no active posts

--- src/components/ListPosts.jsx
+++ src/components/ListPosts.jsx
@@ -3,12 +3,15 @@ import { PostCard } from "../components/index.js";
 
 function ListPosts() {
   const activePosts = useSelector((state) => state.post.activePosts);
+  const activeTopic = useSelector((state) => state.post.activeTopic);
   console.log("active Posts ::", activePosts);
 
   return (
     <div className="flex justify-center flex-wrap w-[90%] mt-10">
-      {!activePosts ? (
-        <h1>No posts yet</h1>
+      {activePosts.length === 0 ? (
+        <h1 className="text-white text-8xl font-serif mb-10 ">
+          No posts yet For {activeTopic}{" "}
+        </h1>
       ) : (
         activePosts.map((post) => (
           <div key={post.$id} className="p-2 w-1/5 ">
