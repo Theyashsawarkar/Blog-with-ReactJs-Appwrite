d222a291cca283dea7683c417aebe8fa2355ec1a
Yash Sawarkar <yashsawarkar01@gmail.com>
2024-05-22 09:49:24 +0530

    highlight active link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { Button, LogoutBtn } from "..";
+import { NavLink, useNavigate } from "react-router-dom";
+import { LogoutBtn } from "..";
 
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
@@ -40,12 +40,19 @@ function Header() {
         <ul className="flex gap-2 mt-3 ml-auto mr-10">
           {navItems.map((item) =>
             item.active ? (
-              <Button
+              <NavLink
                 key={item.name}
-                clickHandler={() => navigate(item.slug)}
-                className="inline-bock mx-2 duration-200 hover:underline hover:text-xl border-white hover:text-[#f97316] "
-                name={item.name}
-              />
+                to={item.slug}
+                className={({ isActive }) =>
+                  `${
+                    isActive
+                      ? "text-[#f97316] font-serif font-bold text-xl duration-200 "
+                      : ""
+                  } inline-bock mx-2 duration-200 hover:underline hover:text-xl border-white hover:text-[#f97316] `
+                }
+              >
+                {item.name}
+              </NavLink>
             ) : null
           )}
           {authStatus && (
