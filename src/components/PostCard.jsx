ef2477486a737b6383f49e1cc5b41c816486c7ef
Yash Sawarkar <yashsawarkar01@gmail.com>
2024-05-25 20:18:19 +0530

    image overflow handled

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -8,20 +8,21 @@ function PostCard({ $id, title, featuredImage }) {
   const navigate = useNavigate();
   return (
     <CCard
-      className="text-white flex flex-col justify-center items-start border rounded-2xl p-5"
+      className="text-white flex flex-col box-border justify-center items-start border rounded-2xl p-5"
       style={{ width: "18rem", height: "18rem" }}
     >
       {featuredImage && (
         <CCardImage
-          className="rounded-2xl outline"
+          className="rounded-2xl outline object-fit"
+          style={{ width: "100%", height: "auto", maxHeight: "10rem" }}
           orientation="top"
           src={appwriteService.getFilePreview(featuredImage)}
         />
       )}
       <CCardBody>
-        <CCardTitle className=" font-semibold my-3">{title}</CCardTitle>
+        <CCardTitle className="font-semibold my-3">{title}</CCardTitle>
         <Button
-          className=" hover:font-semibold duration-300 font-serif bg-[#f97316] hover:bg-white text-black rounded-lg px-2 py-1"
+          className="hover:font-semibold duration-300 font-serif bg-[#f97316] hover:bg-white text-black rounded-lg px-2 py-1"
           shouldPreventDefault={true}
           clickHandler={() => navigate(`/post/${$id}`)}
           name={"Read More.."}
